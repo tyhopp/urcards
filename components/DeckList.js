9e244a00e4a54e2fef51a33d351d2016b7901a23
Ty <thopp@polity.tech>
2017-12-24 13:29:08 -0800

    part way through Flatlist implementation

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native"
+import { Flatlist, StyleSheet, Text, View, TouchableOpacity } from "react-native"
 import { StackNavigator } from "react-navigation"
 import { getDecks } from '../actions'
 import { offWhite, darkTeal } from '../utils/colors'
@@ -12,8 +12,30 @@ import DeckListItemCreator from './DeckListItemCreator'
 
 class DeckList extends Component {
 
-  render() {
+  _keyExtractor = (item, index) => item.id;
+
+  _onPressItem = (id: string) => {
+    // updater functions are preferred for transactional updates
+    this.setState((state) => {
+      // copy the map rather than modifying state.
+      const selected = new Map(state.selected);
+      selected.set(id, !selected.get(id)); // toggle
+      return {selected};
+    });
     const { navigate } = this.props.navigation
+    navigate('DeckCover')
+  };
+
+  _renderItem = ({deck}) => (
+    <DeckListItem
+      id={deck.id}
+      onPressItem={this._onPressItem}
+      selected={!!this.state.selected.get(deck.id)}
+      title={deck.decktitle}
+    />
+  );
+
+  render() {
     const { decks } = this.props
 
     return (
@@ -21,11 +43,11 @@ class DeckList extends Component {
         <TouchableOpacity onPress={() => navigate('DeckCreator')}>
           <DeckListItemCreator />
         </TouchableOpacity>
-        {decks && decks.map((deck) => (
-          <TouchableOpacity key={deck} onPress={() => navigate('DeckCover')}>
-            <DeckListItem deck={deck} />
-          </TouchableOpacity>
-        ))}
+        <Flatlist 
+          data={decks} 
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
       </View>
     );
   }
